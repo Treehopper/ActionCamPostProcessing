05263ec67339b563947bcedfcd9b920a3261b070
Max Hohenegger <github.com@hohenegger.eu>
2014-09-24 01:39:40 +0200

    switched to x86 version of virtual dub

--- deshake.bat
+++ deshake.bat
@@ -12,9 +12,9 @@ for /r %%a in (*.avi) do (
 
 for /F %%a in ('dir /B/D *.avs') do (
 	echo Processing "%%~fa" pass 1... 
-	..\vdub64.exe /s deshake1.vcf /p "%%~fa" "%%~fa.deshaken.avi" /r /c /x 
+	..\vdub.exe /s deshake1.vcf /p "%%~fa" "%%~fa.deshaken.avi" /r /c /x 
 	echo Processing "%%~fa" pass 2... 
-	..\vdub64.exe /s deshake2.vcf /p "%%~fa" "%%~fa.deshaken.avi" /r /c /x
+	..\vdub.exe /s deshake2.vcf /p "%%~fa" "%%~fa.deshaken.avi" /r /c /x
 )
 
 del *.avs
